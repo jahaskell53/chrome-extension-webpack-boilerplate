455ad8f43da7744e624abc1b7663415d283aac35
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-05-02 00:16:21 -0400

    working scroll to part version

--- src/js/popup.js
+++ src/js/popup.js
@@ -16,7 +16,17 @@ document.getElementById("form").addEventListener("submit", (e) => {
 function sendMsg(text) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: text}, function(response) {
-          console.log(response);
+          console.log("response from page", response);
+          addLinkToTarget()
         });
       });
     }
+
+function addLinkToTarget() {
+    const target = document.getElementById("search-target");
+    const link = document.createElement("a");
+    link.href = "#search-target";
+    link.innerText = "Go to target";
+    document.getElementById("links").appendChild(link);
+
+}
--- src/js/script.js
+++ src/js/script.js
@@ -25,8 +25,13 @@ function replaceText(element, word) {
     //   console.log("element is ", element);
       element.childNodes.forEach(childNode => replaceText(childNode, word));
     } else if (element.nodeType === Text.TEXT_NODE) {
+        console.log("Element is", element);
         if (element.textContent.includes(word)) {
+            console.log("Red element is", element);
             setToRed(element)
+            setIdOfElement(element, "search-target")
+            element.parentNode.scrollIntoView(true)
+            return;
         }
         // element.textContent = element.textContent.replace(/chrome/gi, "-----");
     //   if (element.innerHTML.includes("Chrome")) {
@@ -34,6 +39,11 @@ function replaceText(element, word) {
     //     console.log("Red element is", element);
     //   }
     }
+  }
+
+  function setIdOfElement(element, id) {
+    element.parentNode.id = id;
+    // console.log(element);
   }
 
   function sendToServer(text) {
@@ -44,15 +54,15 @@ function replaceText(element, word) {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({query: text}),
+        body: JSON.stringify({query: text, url: document.URL}),
         });
-        
+        console.log("url", document.URL)
         response.then(data => {
         
         //   console.log(data.json());
           data.json().then(data1 => {
-            console.log("response from server", data1);
-            replaceText(document.body, data1)
+            console.log("response from server", data1.test.slice(0, 10));
+            replaceText(document.body, data1.test.slice(0, 10));
           })
         
         
